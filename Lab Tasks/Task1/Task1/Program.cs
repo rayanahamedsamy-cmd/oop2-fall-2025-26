05c9ecf2b697d20bbf8c0c9ff7886c7e244bbeef
NiNET7 <rayanahmedsamy019@gmail.com>
2025-11-04 00:00:10 +0600

    21323

---
+++ Lab Tasks/Task1/Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Enter the Score of Student1: ");
+int St1score = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter the Score of Student1: ");
+int St2score = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter the Score of Student1: ");
+int St3score = Convert.ToInt32(Console.ReadLine());
+
+
+
+double average = (St1score + St2score + St3score) / 3.0;
+Console.WriteLine(average);
---
+++ Lab Tasks/Task2/Task2/Program.cs
@@ -0,0 +1,5 @@
+﻿float ItemPrice = 250.00f;
+float Discount = 15.0f / 100.00f;
+
+float PriceAfterDiscount = ItemPrice - (ItemPrice * Discount);
+Console.WriteLine("The Price After Discount is: " + PriceAfterDiscount + "$");
